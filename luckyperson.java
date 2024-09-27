f37eff40d87c307962eec3d769c554b692001908
Santhosh M <143023757+SanthoshM007@users.noreply.github.com>
2024-09-27 14:33:50 +0530

    Create luckyperson.java

---
+++ luckyperson.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+class luckyperson {
+    public static void main(String[] args) {
+        Scanner o=new Scanner(System.in);
+        int n=o.nextInt();
+        int k=near(n);
+        int v=n-k;
+        System.out.print(2*v+1);
+    }
+    public static int near(int n){
+        int i=1;
+        while(i<=n){
+            i*=2;
+        }
+        i/=2;
+        return i;
+    }
+}
